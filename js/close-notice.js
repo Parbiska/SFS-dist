1d04556427b0c945e0e9da24e104e9ac06c3e5a7
Demid <parbiska@gmail.com>
2023-11-27 01:07:25 +0800

    changes + add notice animation

--- js/close-notice.js
+++ js/close-notice.js
@@ -1,8 +1,6 @@
-const closeNoticeButton = document.querySelector(
-	'.product__purchase-notice-close'
-);
-const notice = document.querySelector('.product__purchase-notice');
+const closeNoticeButton = document.querySelector('.product__notice-close');
+const notice = document.querySelector('.product__notice');
 
 closeNoticeButton.addEventListener('click', () => {
-	notice.style.display = 'none';
+	notice.classList.add('product__notice_hidden');
 });
